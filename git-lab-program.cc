f209848e974d7b44c39118df3798bc9171cad195
Alex Schakel <as760517@ohio.edu>
2020-01-19 18:20:00 -0500

    Basically following the lab #2 prompt

---
+++ git-lab-program.cc
@@ -0,0 +1,22 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Enter your name>
+ *        Date: <Enter today's date>
+ * Description: Add Description
+ */
+
+#include <iostream>
+//***************************//
+// Today's Date: 01/19/2020  //
+// Name: Alexande M. Schakel //
+//***************************//
+
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
